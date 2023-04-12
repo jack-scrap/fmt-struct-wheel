8abb4f80d57d7a3d25d47d1f64c1ec71b16c9b88
Jack <jackhasakeyboard@gmail.com>
2023-04-12 08:14:27 -0600

    Checked number of arguments, none because the time interval is going to be calculated via Unix time

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 const char wheel[2 * 2] = {
 	'-',
 	'\\',
@@ -5,6 +7,16 @@ const char wheel[2 * 2] = {
 	'/'
 };
 
+void err(char* msg) {
+	printf("Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc != 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
 	return 0;
 }
